4f178ef4fb62627f69ae09f1284ce220e99ca6d3
zhaobinxu23 <77838473+zhaobinxu23@users.noreply.github.com>
2021-11-27 13:35:51 +0800

    Delete new_align_fast.m

--- new_align_fast.m
+++
@@ -1,8 +0,0 @@
-function count_number = new_align_fast(new_sequence1,new_sequence2)
-count_number = 0;
-for i = 1:length(new_sequence1)
-    if new_sequence1(i) ~= new_sequence2(i)
-        count_number = count_number + 1;
-    end
-end
-end
